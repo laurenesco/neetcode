44222ae2f234090a20659441297abc4e1aadf37b
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-17 18:16:20 -0500

    Create minimum_window_substring.py

---
+++ sliding_window/minimum_window_substring.py
@@ -0,0 +1,43 @@
+# Memory: 7.9 MB•Time: 29ms•Submitted at: 07/17/2026 18:14
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        need_dict = {}
+        have_dict = {}
+        l, have, length = 0, 0, 0
+        result = ''
+
+        # Initialize need_dict
+        for c in t:
+            need_dict[c] = need_dict.get(c, 0) + 1
+
+        need = len(need_dict)
+
+        # Iterate over have_dict
+        for r in range(len(s)):
+
+            # If the char is in t, increment count in have_dict
+            if s[r] in need_dict:
+
+                # Increment have_dict count, necessarily
+                have_dict[s[r]] = have_dict.get(s[r], 0) + 1
+
+                # Increment have, if necessary
+                if need_dict[s[r]] == have_dict[s[r]]:
+                    have += 1
+
+                # Save substring, if necessary
+                while need == have:
+                    if length == 0 or r - l + 1 < length:
+                        result = [l, r]
+                        length = r - l + 1
+
+                    if s[l] in have_dict:
+                        have_dict[s[l]] -= 1
+                        if have_dict[s[l]] < need_dict[s[l]]:
+                            have -= 1
+
+                    l += 1
+
+        return s[result[0]:result[1] + 1] if result else ''
