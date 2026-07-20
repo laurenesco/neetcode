0940afe88b28665ba5c08fe3f07386c2818068a0
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-20 12:32:34 -0500

    Update task_scheduler.py

--- heap_priority_queue/task_scheduler.py
+++ heap_priority_queue/task_scheduler.py
@@ -1,5 +1,13 @@
 # Memory: 8 MB•Time: 58ms•Submitted at: 07/19/2026 13:08
 
+# You are given an array of CPU tasks tasks, where tasks[i] is an uppercase english character from A to Z. You are also given an integer n.
+
+# Each CPU cycle allows the completion of a single task, and tasks may be completed in any order.
+
+# The only constraint is that identical tasks must be separated by at least n CPU cycles, to cooldown the CPU.
+
+# Return the minimum number of CPU cycles required to complete all tasks.
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         occurences = self.initOccurences(tasks)
