466d6452801360251192483189fda1b49b37a8b6
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-20 12:28:55 -0500

    Update two_sum.py

--- arrays_and_hashing/two_sum.py
+++ arrays_and_hashing/two_sum.py
@@ -1,6 +1,12 @@
 # Memory: 7.7 MB•Time: 27ms•Submitted at: 04/15/2026 14:14
 # Beats 100% in runtime, 99.97% in memory
 
+# Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+# You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+# Return the answer with the smaller index first. 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = []
