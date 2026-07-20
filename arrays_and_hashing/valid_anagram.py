700e42f1faed3f144a32678b7cf62d043de7bf12
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-20 12:29:54 -0500

    Update valid_anagram.py

--- arrays_and_hashing/valid_anagram.py
+++ arrays_and_hashing/valid_anagram.py
@@ -1,6 +1,10 @@
 # Memory: 8.7 MB•Time: 36ms•Submitted at: 04/09/2026 13:23
 # Beats 72.51% in runtime, 23.50% in memory
 
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # Approach: Sort the provided strings, compare the 
