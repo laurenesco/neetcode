b202b9886cfc98497462266bf9ca5f1c08c493de
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-20 12:32:10 -0500

    Update min_cost_climbing_stairs.py

--- 1-d_dynamic_programming/min_cost_climbing_stairs.py
+++ 1-d_dynamic_programming/min_cost_climbing_stairs.py
@@ -1,5 +1,11 @@
 # Memory: 8 MB•Time: 28ms•Submitted at: 07/19/2026 17:54
 
+# You are given an array of integers cost where cost[i] is the cost of taking a step from the ith floor of a staircase. After paying the cost, you can step to either the (i + 1)th floor or the (i + 2)th floor.
+
+# You may choose to start at the index 0 or the index 1 floor.
+
+# Return the minimum cost to reach the top of the staircase, i.e. just past the last index in cost.
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
