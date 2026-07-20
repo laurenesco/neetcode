417685ff4fdf1f357a9afc95d85ab1000ccde3af
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-20 12:30:34 -0500

    Update encode_and_decode_strings.py

--- arrays_and_hashing/encode_and_decode_strings.py
+++ arrays_and_hashing/encode_and_decode_strings.py
@@ -1,6 +1,8 @@
 # Memory: 8 MB•Time: 29ms•Submitted at: 04/16/2026 15:12
 # Beats 99.76% in runtime, 98.99% in memory
 
+Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
