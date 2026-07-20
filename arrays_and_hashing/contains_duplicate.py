03055094b901f02b808db57e09710d623f6fcd53
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-20 12:30:13 -0500

    Update contains_duplicate.py

--- arrays_and_hashing/contains_duplicate.py
+++ arrays_and_hashing/contains_duplicate.py
@@ -1,6 +1,8 @@
 # Memory: 8.2 MB•Time: 45ms•Submitted at: 04/08/2026 13:27
 # Beats 7.01% in runtime, 2.26% in memory
 
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         # Map approach, if value exists return false
