218adfb3e4a9f8d5bbf1043fed7130e6f6aee782
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-21 13:09:06 -0500

    Update spiral_matrix.py

--- math_and_geometry/spiral_matrix.py
+++ math_and_geometry/spiral_matrix.py
@@ -1,3 +1,5 @@
+# Memory: 7.9 MB•Time: 31ms•Submitted at: 07/21/2026 13:08
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
 
@@ -10,23 +12,33 @@ class Solution:
         while top <= bottom and left <= right:
 
             # Move left to right
+            print("traversing left to right")
             for i in range(left, right + 1):
                 result.append(matrix[top][i])
+                print(matrix[top][i])
             top += 1
 
             # Move down
+            print("traversing down")
             for i in range(top, bottom + 1):
                 result.append(matrix[i][right])
+                print(matrix[i][right])
             right -= 1
 
             # Move right to left
-            for i in range(right, left - 1, -1):
-                result.append(matrix[bottom][i])
-            bottom -= 1
+            print("traversing right to left")
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                    print(matrix[bottom][i])
+                bottom -= 1
 
             # Move up
-            for i in range(bottom, top - 1, -1):
-                result.append(matrix[i][left])
-            left += 1
+            print("traversing up")
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                    print(matrix[i][left])
+                left += 1
 
         return result
